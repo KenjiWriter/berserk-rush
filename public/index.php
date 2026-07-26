0db4cc0f311f81f1425daeb3397eeed732b3e13b
Maciej Wiśniewski <maciej.wisniewski@aol.com>
2026-07-26 10:59:05 +0200

    feat: initialize public entry point with custom memory limit configuration

--- public/index.php
+++ public/index.php
@@ -5,6 +5,10 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Increase memory limit for heavy Blade/Livewire renders
+ini_set('memory_limit', '256M');
+
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
